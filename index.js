570ad6a5a92ede3c1cd15dfa59c38da8f9060d34
wakababa <altgblbl@gmail.com>
2020-12-09 14:47:16 +0300

    s

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Command working")
+var createApi = require('./generate-api/index')
+
+createApi({
+    apiname:"user",
+    prop:{
+        name:"String",
+        age:"Number"
+    },
+    url:"mongodb+srv://waka:waka@cluster0.jtleo.mongodb.net/test1"
+
+})
