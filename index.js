98cd7c9a9bf2bc6502c6d846df278d1feb38ca24
wakababa <altgblbl@gmail.com>
2022-11-18 22:52:24 +0300

    npx added

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var { createApi } = require("./generate-api/index");
+module.exports = {createApi};
+
+var inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {type:'input',message:'Api Name :' ,name:'apiname' },
+        {type:'input',message:'Url :' ,name:'url' },
+        {type:'input',message:'Add Database :' ,name:'prop' },
+    ])
+    .then(answers => {
+        createApi({
+            apiname:answers.apiname,
+            url:answers.url,
+            prop:JSON.parse(answers.prop)
+        })
+    });
