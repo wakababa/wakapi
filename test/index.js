37b4c2276f97c659651a3a95e6a0e812a315c5d5
wakababa <altgblbl@gmail.com>
2022-11-18 22:15:34 +0300

    a

--- test/index.js
+++ test/index.js
@@ -1,16 +1,10 @@
-#!/usr/bin/env node
 import { createApi } from "npm:wakapi";
-import  prompt from 'npm:inquirer';
-
-prompt([
-   {type:'input',message:'Api Name :' ,name:'apiname' },
-   {type:'input',message:'Url :' ,name:'url' },
-   {type:'input',message:'Add Database :' ,name:'prop' },
-  ])
-  .then(answers => {
-    createApi({
-      apiname:answers.apiname,
-      url:answers.url,
-      prop:JSON.parse(answers.prop)
-    })
-  });
+createApi({
+  apiname:"user",
+  url:"mongodb+srv://waka:waka@cluster0.jtleo.mongodb.net/?retryWrites=true&w=majority",
+  prop:{
+    name:"String",
+    age:"Number",
+    job:"String"
+  }
+})
